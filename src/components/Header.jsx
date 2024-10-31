cdf5561c1a07a0c857b439857b94bbba80f8bbe6
meclozano <container.me@gmail.com>
2024-10-31 09:25:47 -0500

    trabajo4

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import ContentButtons from './componentsHeader/ContentButtons'
+
+function Header() {
+    
+    const buttons = {
+        addTaks: {text:'Agregar tarea', value: true},
+        showTaks: {text: 'Ver tareas', value: false}
+    }
+    
+  return (
+    <section>       
+        <h1>MANEJA TUS TAREAS</h1> 
+        <ContentButtons objectButtons={buttons}></ContentButtons>
+    </section>
+    
+  )
+}
+
+Header.propTypes = {}
+
+export default Header
